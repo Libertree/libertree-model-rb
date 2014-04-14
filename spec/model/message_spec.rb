Libertree/libertree

commit f731af26eb8e505df88725ded54a96729be95572

move distribution job creation from frontend here

lib/libertree/model/message.rb
@@ -6,6 +6,29 @@ module Libertree
       end
       alias :member :sender
 
+      def distribute
+        trees = self.recipients.reduce(Set.new) { |_trees, recipient|
+          if recipient.tree
+            _trees << recipient.tree
+          end
+          _trees
+        }
+        recipient_ids = self.recipients.map(&:id)
+
+        trees.each do |tree|
+          Libertree::Model::Job.create(
+            {
+              task: 'request:MESSAGE',
+              params: {
+                'message_id'           => self.id,
+                'server_id'            => tree.id,
+                'recipient_member_ids' => recipient_ids,
+              }.to_json,
+            }
+          )
+        end
+      end
+
       def recipients
         return @recipients  if @recipients
         @recipients = Member.s(
@@ -40,6 +63,7 @@ module Libertree
           sender_member_id: args[:sender_member_id],
           text: args[:text]
         )
+        message.distribute
 
         sender_member = Model::Member[ args[:sender_member_id].to_i ]
 

lib/libertree/model/chat-message.rb
@@ -1,6 +1,24 @@
 module Libertree
   module Model
     class ChatMessage < Sequel::Model(:chat_messages)
+      def after_create
+        super
+        self.distribute
+      end
+
+      def distribute
+        return  if ! self.recipient.tree
+        Libertree::Model::Job.create(
+          {
+            task: 'request:CHAT',
+            params: {
+              'chat_message_id' => self.id,
+              'server_id'       => self.recipient.tree.id,
+            }.to_json,
+          }
+        )
+      end
+
       def sender
         @sender ||= Member[self.from_member_id]
       end

Libertree/libertree-model-rb

commit ba6c845f6accf76f86a03ee3b520dfa2e408c192

fix message distribution
Message distribution was broken by me with commit
f731af26eb8e505df88725ded54a96729be95572

Obviously, we can only distribute the message once all recipients have
been recorded, not right after the message record alone has been
created.

Added distribution tests to make sure this won't happen again.

lib/libertree/model/message.rb
@@ -106,8 +106,6 @@ module Libertree
           sender_member_id: args[:sender_member_id],
           text: args[:text]
         )
-        message.distribute
-
         sender_member = Model::Member[ args[:sender_member_id].to_i ]
 
         recipient_member_ids = Array(args[:recipient_member_ids])
@@ -132,7 +130,7 @@ module Libertree
             end
           end
         end
-
+        message.distribute
         message
       end
 

spec/model/message_spec.rb
@@ -33,6 +33,27 @@ describe Libertree::Model::Message do
     end
   end
 
+  describe 'distribute' do
+    it 'creates a distribution job for each remote server' do
+      expect( Libertree::Model::Job ).to receive(:create) do |args|
+        expect( args[:task] ).to eq('request:MESSAGE')
+      end
+      message = Libertree::Model::Message.
+        create_with_recipients({ :sender_member_id => @member.id,
+                                 :recipient_member_ids => [@remote_member.id],
+                                 :text => 'Hello'
+                               })
+    end
+    it 'creates no distribution job for messages to local members' do
+      expect( Libertree::Model::Job ).not_to receive(:create)
+      message = Libertree::Model::Message.
+        create_with_recipients({ :sender_member_id => @member.id,
+                                 :recipient_member_ids => [@local_member.id],
+                                 :text => 'Hello'
+                               })
+    end
+  end
+
   before :each do
     Libertree::DB.dbh[ "TRUNCATE messages, message_recipients" ].get
     @message_sent = Libertree::Model::Message.